1a383ec19c90598f41dcaced77d7301df87ec79d
Bill Normandin <bill@normandindev.net>
2016-04-21 19:03:21 -0500

    Application Configuration tool prototype completed

--- resources/frameworks/configure.py
+++ resources/frameworks/configure.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import time
 
 # Colorize console output
 from pokeyworks import Color, color_wrap
@@ -9,115 +10,151 @@ from pokeyworks import Color, color_wrap
 from pokeyworks import PokeyConfig
 
 
-def configure_application():
-    # Routine to create or edit configuration files
-    # to be saved in JSON or YAML format using the
-    # pokeyworks.PokeyConfig() class.
-    print_app_header()
+class AppConfig():
 
-    menu_queue = []
-    highlight = Color.BLUE
-    # Menu option groups :
-    submenu_1 = [
-            ('View options',['v',highlight],print_config),
-            ('Add an option',['a',highlight],add_option),
-            ('Edit an option',['e',highlight],edit_option),
-            ('Remove an option',['r',highlight],remove_option),
-            ('Write Config',['w',highlight],write_config),
-            ('Back',['b',Color.RED],None),
-            ]
-    main_menu = [
-            ('Open a file',['o',highlight],file_select,submenu_1),
-            ('New config file',['n',highlight],create_config),
-            ('Convert a file',['c',highlight],convert_file),
-            ('Quit',['q',highlight],sys.exit,0),
-            ]
+    def __init__(self):
+        self.print_app_headers()
 
-    while True:
-        # Main application execution loop
-        try:
-            
-        except SystemExit,KeyboardInterrupt:
-            ch = raw_input('Really quit? ')
-            if ch.upper()=='Y':
-                sys.exit(0)
+        self.cfg = None
+        menu_queue = []
+        highlight = Color.BLUE
+        # Menu option groups :
+        submenu_1 = [
+                ('View options',['v',highlight],self.print_config,[self.cfg]),
+                ('Add an option',['a',highlight],self.add_option,[self.cfg]),
+                ('Edit an option',['e',highlight],self.edit_option,[self.cfg]),
+                ('Remove an option',['r',highlight],self.remove_option,[self.cfg]),
+                ('Write Config',['w',highlight],self.write_config,[self.cfg]),
+                ('Back',['b',Color.RED],None),
+                ]
+        main_menu = [
+                ('Open a file',['o',highlight],self.file_select,[submenu_1]),
+                ('New config file',['n',highlight],self.create_config,[self.cfg]),
+                ('Convert a file',['c',highlight],self.convert_file,[]),
+                ('Quit',['q',highlight],sys.exit,[0]),
+                ]
+
+        while True:
+            # Main application execution loop
+            try:
+                pass
+            except SystemExit,KeyboardInterrupt:
+                ch = raw_input('Really quit? ')
+                if ch.upper()=='Y':
+                    sys.exit(0)
+                else:
+                    continue
+
+    def print_app_headers(self):
+        header_path = 'app_header.txt'
+        with open(header_path, 'r') as f:
+            data = f.readlines()
+
+        for row in data:
+            time.sleep(0.05)
+            print row.rstrip()
+
+    def print_config(self):
+        print 'Loaded Options :'
+        for key in self.self.cfg.conf_dict:
+            print ' [{}] = {}'.format(key,self.cfg.conf_dict)
+
+    def add_option(self):
+        key = raw_input('Option : ')
+        val = raw_input('Value  : ')
+        self.cfg.conf_dict[key]=val
+
+    def edit_option(self):
+        while True:
+            print_config(self.cfg)
+            print '[Edit Option]'
+            key = raw_input('Selection (q quits) > ')
+            if key.upper() in ['Q','QUIT','EXIT']:
+                break
+            if key in self.cfg.conf_dict:
+                self.cfg.conf_dict[key] = raw_input('Value > ')
             else:
-                continue
+                msg = 'Option {} does not exist, create it? '.format(key)
+                ch=raw_input(msg)
+                if ch.upper() in ['Y','YES']:
+                    self.cfg.conf_dict[key]=raw_input('Value > ')
 
-def print_config(cfg):
-    print 'Loaded Options :'
-    for key in cfg.conf_dict:
-        print ' [{}] = {}'.format(key,cfg.conf_dict)
-
-def add_option(cfg):
-    key = raw_input('Option : ')
-    val = raw_input('Value  : ')
-    cfg.conf_dict[key]=val
-
-def edit_option(cfg):
-    while True:
-        print_config(cfg)
-        print '[Edit Option]'
+    def remove_option(self,opt=None):
+        if opt is not None and opt in self.cfg.conf_dict:
+            del self.cfg.conf_dict[opt]
+            return
+        elif opt is not None:
+            print "Option not in list!"
+        self.print_config(cfg)
         key = raw_input('Selection (q quits) > ')
         if key.upper() in ['Q','QUIT','EXIT']:
-            break
-        if key in cfg.conf_dict:
-            cfg.conf_dict[key] = raw_input('Value > ')
-        else:
-            msg = 'Option {} does not exist, create it? '.format(key)
-            ch=raw_input(msg)
+            return
+        elif key in self.cfg.conf_dict:
+            del self.cfg.conf_dict[key]
+            return
+        print 'Option not found!'
+
+    def write_config(self):
+        self.cfg.write_cfg()
+
+    def file_select(self):
+        fname = raw_input("Enter a path : ")
+        try:
+            finfo = os.stat(fname)
+            print fname
+            print finfo
+            ch = raw_input("Load this config? ")
             if ch.upper() in ['Y','YES']:
-                cfg.conf_dict[key]=raw_input('Value > ')
+                if fname.endswith('.json'):
+                    self.cfg = pokeyworks.PokeyConfig(fname)
+                elif fname.endswith('.yaml'):
+                    self.cfg = pokeyworks.PokeyConfig(fname,2)
+                else:
+                    raise AssertionError("Unknown file type : {}".format(fname))
+        except OSError:
+            print "File not found! {}".format(fname)
 
-def remove_option(cfg,opt=None):
-    if opt is not None and opt in cfg.conf_dict:
-        del cfg.conf_dict[opt]
-        return
-    elif opt is not None:
-        print "Option not in list!"
-    print_config(cfg)
-    key = raw_input('Selection (q quits) > ')
-    if key.upper() in ['Q','QUIT','EXIT']:
-        return
-    elif key in cfg.conf_dict:
-        del cfg.conf_dict[key]
-        return
-    print 'Option not found!'
-
-def write_config(cfg):
-    cfg.write_cfg()
-
-def file_select(cfg):
-    fname = raw_input("Enter a path : ")
-    try:
-        finfo = os.stat(fname)
-        print fname
-        print finfo
-        ch = raw_input("Load this config? ")
+    def create_config(self):
+        fname = raw_input("Config file name : ")
+        if fname.endswith(".json"):
+            args = [fname]
+        elif fname.endswitch(".yaml"):
+            args = [fname,2]
+        else:
+            print "Invalid file type : {}".format(fname)
+            return False
+        try:
+            with open(fname,'x') as touchfile:
+                #Touch!
+                pass
+        except IOError:
+            print "Unable to stat file : {} - check your permissions foo!".format(fname)
+            return False
+        self.cfg = PokeyConfig(*args)
+        ch = raw_input("Config file created!  Add options? ")
         if ch.upper() in ['Y','YES']:
-            if fname.endswith('.json'):
-                cfg = pokeyworks.PokeyConfig(fname)
-            elif fname.endswith('.yaml'):
-                cfg = pokeyworks.PokeyConfig(fname,2)
-            else:
-                raise AssertionError("Unknown file type : {}".format(fname))
-    except OSError:
-        print "File not found! {}".format(fname)
+            return 1    # Need to catch this and move to submenu 1
+        else:
+            return 0
 
-def create_config(cfg,opt):
-    fname = raw_input("Config file name : ")
-    if fname.endswith(".json"):
+    def convert_file(self,fpath):
+        if self.cfg.loaded_type == PokeyConfig.json:
+            self.cfg.fpath = self.cfg.convert_file_path(fpath,'.yaml')
+            self.cfg.save_yaml(self.cfg.fpath,self.cfg.conf_dict)
+            self.cfg.loaded_type = PokeyConfig.yaml
+        elif self.cfg.loaded_type == PokeyConfig.yaml:
+            self.cfg.fpath = self.cfg.convert_file_path(fpath,'.json')
+            self.cfg.save_json(self.cfg.fpath,self.cfg.conf_dict)
+            self.cfg.loaded_type = PokeyConfig.json
+        else:
+            raise AssertionError("Unknown type : {}".format(self.self.cfg.loaded_type))
 
-    elif fname.endswitch(".yaml"):
+        print "{} converted to {}".format(fpath,self.cfg.fpath)
+    def menu(opts):
+        # Prints menu options and handles user selections
 
-    else:
-        print "Invalid file type : {}".format(fname)
-def convert_file():
-def menu(opts):
-    # Prints menu options and handles user selections
-
-    while True:
-        
+        while True:
+            break 
 
 if __name__=='__main__':
-    configure_application()
+    AppConfig()
